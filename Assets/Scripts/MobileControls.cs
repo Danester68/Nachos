611f31a779a1a64163817f58c393b9844c5dd173
Danester68 <kjdyson68@gmail.com>
2026-01-26 10:55:58 -0500

    Add MobileControls and MobileControls.cs

---
+++ Assets/Scripts/MobileControls.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MobileControls : MonoBehaviour
+{
+    [SerializeField] GameObject mobileControlsObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        #if UNITY_IOS || UNITY_ANDROID || UNITY_WEBGL
+            mobileControlsObject.SetActive(true);
+        #else
+            mobileControlsObject.SetActive(false);
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
